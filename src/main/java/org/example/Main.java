12a5c6b9ca2f9edc9116ed791528f86edba6550f
Shaik-Mohammad-Kaif <skmdkaif321@gamil.com>
2026-09-23 15:43:25 +0530

    feat: implement add and view job applications

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
+
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("==================================");
+        System.out.println("     JOB TRACKER APPLICATION");
+        System.out.println("==================================");
+
+        System.out.println();
+
+        // Create a job application object
+        Job job1 = new Job(
+                "Google",
+                "Java Developer",
+                "https://careers.google.com",
+                "Applied",
+                "2026-09-23",
+                true
+        );
+
+        // Display job details
+        System.out.println(job1);
+
     }
 }
